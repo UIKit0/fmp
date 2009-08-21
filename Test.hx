b8da87080a34723e8c451d786df6a005aa1d676b
badsector <badsector@runtimeterror.com>
2009-08-22 02:59:58 +0300

    Modified .gitignore to ignore MOD files
    I regularly use MOD files for testing and keep them in the source directory. However, as one can imagine, i don't want them to be commited to the repository, so i add this entry in .gitignore to ignore these files. You really don't want to hear what i'm listening to while trying to debug the player - and also i don't want you to hear MODs that play incorrectly :-).
    
    Beyond this small addition in the .gitignore, i also commit Test.hx because git keeps nagging me about it being modified and such. I didn't really modified it. Ok, i did. But only a little. I only changed the filename of the MOD to load. But is this really a good reason to nag me git? Is it?
    
    I wonder...

--- Test.hx
+++ Test.hx
@@ -30,10 +30,10 @@ class Test
 {
     static var pb:flash.display.Sprite;
     static var mp:ModPlayer;
-	static var te:flash.text.TextField;
-	static var musix:Array<String>;
-	static var curmus:Int = 0;
-	static var lastclick:Float = 0;
+    static var te:flash.text.TextField;
+    static var musix:Array<String>;
+    static var curmus:Int = 0;
+    static var lastclick:Float = 0;
     
     static function setProgress(prg:Int)
     {
@@ -41,53 +41,53 @@ class Test
         pb.graphics.beginFill(0x5ec4ee);
         pb.graphics.drawRect(0, 0, 128*prg/100, 48);
         pb.graphics.endFill();
-		if (prg == 100) {
-			te.text = musix[curmus] + "\n\nClick to change";
-		} else te.text = "\n" + prg + "%";
+        if (prg == 100) {
+            te.text = musix[curmus] + "\n\nClick to change";
+        } else te.text = "\n" + prg + "%";
     }
-	
-	/* ...with extra code for click-crazy people! */
-	static function onteclick(ev:MouseEvent)
-	{
-		mp.stop();
-		if (haxe.Timer.stamp() - lastclick < 0.2) {
-			te.text = "\nDONT DO THIS!!!";
-			lastclick = haxe.Timer.stamp();
-			return;
-		}
-		lastclick = haxe.Timer.stamp();
-		curmus++;
-		if (curmus > musix.length) curmus = 0;
-		if (curmus < musix.length) {
-			mp = new ModPlayer();
-			mp.onProgress = function(prg:Int) { setProgress(prg); }
-			flash.media.SoundMixer.stopAll();
-			mp.play(musix[curmus]);
-		}
-		else te.text = "Stopped\n\nClick to start";
-	}
+    
+    /* ...with extra code for click-crazy people! */
+    static function onteclick(ev:MouseEvent)
+    {
+        mp.stop();
+        if (haxe.Timer.stamp() - lastclick < 0.2) {
+            te.text = "\nDONT DO THIS!!!";
+            lastclick = haxe.Timer.stamp();
+            return;
+        }
+        lastclick = haxe.Timer.stamp();
+        curmus++;
+        if (curmus > musix.length) curmus = 0;
+        if (curmus < musix.length) {
+            mp = new ModPlayer();
+            mp.onProgress = function(prg:Int) { setProgress(prg); }
+            flash.media.SoundMixer.stopAll();
+            mp.play(musix[curmus]);
+        }
+        else te.text = "Stopped\n\nClick to start";
+    }
 
     static function main()
     {
         pb = new flash.display.Sprite();
-		pb.addEventListener(MouseEvent.CLICK, onteclick);
+        pb.addEventListener(MouseEvent.CLICK, onteclick);
         flash.Lib.current.addChild(pb);
-		
-		te = new TextField();
-		te.width = 128;
-		te.height = 48;
-		te.autoSize = flash.text.TextFieldAutoSize.CENTER;
-		te.addEventListener(MouseEvent.CLICK, onteclick);
-		flash.Lib.current.addChild(te);
-		
-		try {
-			var musicfiles:String = flash.Lib.current.loaderInfo.parameters.modfile;
-			musix = musicfiles.split(",");
-		} catch (whatever:Dynamic) {
-			musix = ["direction.mod"];
-		}
         
-		lastclick = haxe.Timer.stamp();
+        te = new TextField();
+        te.width = 128;
+        te.height = 48;
+        te.autoSize = flash.text.TextFieldAutoSize.CENTER;
+        te.addEventListener(MouseEvent.CLICK, onteclick);
+        flash.Lib.current.addChild(te);
+        
+        try {
+            var musicfiles:String = flash.Lib.current.loaderInfo.parameters.modfile;
+            musix = musicfiles.split(",");
+        } catch (whatever:Dynamic) {
+            musix = ["game-menu.mod"];
+        }
+        
+        lastclick = haxe.Timer.stamp();
         mp = new ModPlayer();
         mp.onProgress = function(prg:Int) { setProgress(prg); }
         mp.play(musix[0]);
